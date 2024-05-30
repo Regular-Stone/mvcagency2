d0d40d96bda00280fccdb5ceaffa5e1f851c7b11
Regular-Stone <132766689+Regular-Stone@users.noreply.github.com>
2024-05-30 21:10:48 +0200

    Delete core/config/routes.php

--- core/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-// roads.php
-
-return [
-    'HomeController' => ['index', 'about', 'contact'],
-    'UserController' => ['login', 'register', 'logout'],
-    'AnnonceController' => ['index', 'addAnnonce', 'registerAnnonce', 'showAnnonce', 'deleteAnnonce'],
-    // Ajoutez d'autres contrôleurs et actions ici
-];
